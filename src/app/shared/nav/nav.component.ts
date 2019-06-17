5b2b6f677626be35368a45301a90a3339609e6d1
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2019-06-16 20:45:48 -0400

    clean up

---
+++ src/app/shared/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import rulesData from './../../../data/rules.json';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+
+  data = rulesData;
+
+  constructor() { }
+
+}
